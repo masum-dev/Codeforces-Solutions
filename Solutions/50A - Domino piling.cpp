d2a5e6540bfd3c2ee496b856d8ccd821702cad5f
masum <mail.masum.dev@gmail.com>
2023-10-23 14:01:22 +0600

    Added solution for 50A - Domino piling | greedy, math

---
+++ Solutions/50A - Domino piling.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+int main() {
+ 
+    int m, n, max_domino;
+    cin >> m >> n;
+ 
+    max_domino = (m * n) / 2;
+ 
+    cout << max_domino << endl;
+    
+    return 0;
+}
